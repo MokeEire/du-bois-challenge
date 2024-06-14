ddeb7aacfe6f8184426d2c7d27e887be430a8425
Mark B <markkbarrett@gmail.com>
2024-06-14 11:42:09 -0700

    chore: Update plate layout styling and legend for challenge01

--- src/routes/plates/challenge01/Legend.svelte
+++ src/routes/plates/challenge01/Legend.svelte
@@ -1,42 +1,61 @@
 <script>
-    export let colourScale;
-    export let indexStart = 0;
-    export let indexStop = colourScale.domain().length;
+  export let colourScale;
+  export let indexStart = 0;
+  export let indexStop = colourScale.domain().length;
+  export let right = false;
 
-    console.log(colourScale.domain());
-    let indexedArray = colourScale.domain().slice(indexStart, indexStop)
+  console.log(colourScale.domain());
+  let indexedArray = colourScale.domain().slice(indexStart, indexStop);
 
-    console.log(indexedArray);
+  function labelLegend(value) {
+    let mapper = {
+      "20000 - 30000": "Between 20,000 and 30,000",
+      "15000 - 20000": "15,000 to 20,000",
+      "10000 - 15000": "10,000 to 15,000",
+      "5000 - 10000": "5,000 to 10,000",
+      "2500 - 5000": "2,500 to 5,000",
+      "1000 - 2500": "1,000 to 2,500",
+      "> 1000": "Under 1,000",
+    };
+    return mapper[value];
+  }
+
+  console.log(indexedArray);
 </script>
 
-<div class='legend'>
-    {#each indexedArray.reverse() as group}
-      <p>
-        <span style="background-color: {colourScale(group)}" />
-        {group}
-      </p>
-    {/each}
-  </div>
+<div class="legend" class:right>
+  {#each indexedArray.reverse() as group}
+    <p>
+      <span style="background-color: {colourScale(group)}" />
+      {labelLegend(group)}
+    </p>
+  {/each}
+</div>
 
 <style>
-    .legend {
-        display: flex;
-        flex-direction: column;
-        align-items: flex-start;
-        flex-grow: 3;
-        height: 50%;
-        justify-content: space-around;
-        padding: 16px 0;
-    }
-    .legend p {
-        font-size: 2rem;
-        font-weight: 100;
-    }
-    span {
-        width: 16px;
-        height: 16px;
-        display: inline-block;
-        border: 1px solid black;
-        border-radius: 50%;
-    }
+  .legend {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    padding: 16px 0;
+    gap: 24px;
+    top: 24px;
+    position: relative;
+    min-width: 40%;
+  }
+
+  .legend.right {
+    left: 10%;
+  }
+  .legend p {
+    font-size: 16px;
+    font-weight: 100;
+  }
+  span {
+    width: 24px;
+    height: 24px;
+    display: inline-block;
+    border: 1px solid black;
+    border-radius: 50%;
+  }
 </style>
