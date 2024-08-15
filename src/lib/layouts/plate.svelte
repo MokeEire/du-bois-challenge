1bb2fe13c6558ade8ce9ce2d112d01fc32153fb2
Mark B <markkbarrett@gmail.com>
2024-08-15 12:35:51 -0700

    Style the plate header and revise disclaimer

--- src/lib/layouts/plate.svelte
+++ src/lib/layouts/plate.svelte
@@ -2,6 +2,8 @@
   import Seo from "$lib/components/Seo.svelte";
   import Categories from "$lib/components/Categories.svelte";
   import { siteTitle } from "$lib/constants";
+  import { base } from "$app/paths";
+  import {format} from "date-fns";
 
   export let data;
   export let title,
@@ -20,33 +22,61 @@
   };
 
   $: recentPosts = data.plates;
+  $: actualDate = new Date(date);
 </script>
 
 <Seo {...seo} />
 
-<h1>Challenge #{challenge}: {title} (Plate {plateNum})</h1>
-<p class="date">{date}</p>
+<a class="challenge" href="https://github.com/ajstarks/dubois-data-portraits/tree/master/challenge/2024#the-challenges">Challenge #{challenge}</a>
+<h1>{title} (Plate {plateNum})</h1>
+
+<p class="date">{format(actualDate, "d MMM yyyy")}</p>
+
+
 <blockquote>
   <strong>Please Note</strong>: The language used in this piece comes directly
-  from W.E.B. Du Bois' scientific study in 1900. You can view the original piece
-  <a href={original}>here</a>. I am torn about reproducing language that could
+  from W.E.B. Du Bois' scientific study in 1900. I am torn about reproducing language that could
   be painful for people to read, but I thought altering the wording would be a
   disservice to the work. I find the language of Du Bois' work to be a powerful
   reminder, especially for readers in a modern context, that racial inequality
   has a long tenure in our societies and institutions. However, race is a
-  difficult subject for white people like myself to navigate with appropriate
-  awareness and compassion, so <a href="../contact">please reach out</a> with any
-  thoughts or feedback on how to better handle the subject.
+  difficult subject to navigate with appropriate
+  awareness and compassion, especially for white people like myself. If you have any
+  thoughts or feedback on how to better present this work, <a href="../contact">please reach out</a>.
 </blockquote>
 <slot />
+
+    <a class="padding-bottom border-top" href="/" sveltekit:prefetch>&#8592; Back to all plates </a>
 <p class="edit"><a href={edit} target="_blank">Edit this page</a></p>
 
 <style>
+
+  .challenge {
+    font-size: 0.95rem;
+    color: var(--text-color);
+    opacity: 0.8;
+    margin-bottom: 0;
+    text-decoration: none;
+    font-weight: 500;
+  }
+
+  .challenge:hover {
+    text-decoration: underline;
+  }
+
+  h1 {
+    margin-top: 0;
+    font-size: 2rem;
+  }
   .date {
     font-size: 1rem;
     color: var(--text-color);
     opacity: 0.8;
     margin-bottom: 0.5rem;
+  }
+
+  .original {
+    font-size: 1rem;
   }
   .edit {
     display: flex;
@@ -58,6 +88,16 @@
     object-fit: cover;
     object-position: center;
   }
+
+  .border-top {
+  border-top: 1px solid var(--accent-color);
+  border-top: 0.5px solid var(--accent-color);
+  padding-top: 1.5rem;
+}
+
+.padding-bottom {
+  padding-bottom: 0.2rem;
+}
 
   blockquote {
     font-size: 0.9rem;
