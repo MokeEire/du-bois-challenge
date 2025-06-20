c8186caa2caa006c8bc61a206499498f11085875
Mark Barrett <markkbarrett@gmail.com>
2025-06-20 12:47:30 -0400

    Add challenge 3 files

---
+++ src/routes/plates/challenge03/+page.md
@@ -0,0 +1,78 @@
+---
+layout: plate
+challenge: 3
+title: "Acres of Land Owned by Negroes in Georgia"
+plateNum: 6
+description: "A graph depicting growth in black wealth in the late 1800s."
+categories:
+  - dataviz
+date: 2025-06-18
+published: true
+image: /plates/challenge03/maps.png
+original: https://github.com/ajstarks/dubois-data-portraits/blob/master/challenge/2024/challenge03/original-plate-19.jpg
+---
+
+<script>
+  import Chart from './Chart.svelte'
+</script>
+
+<div class="plate">
+  <div class="chart-title">
+    <h1>Acres of Land Owned by Negroes in Georgia.</h1>
+  </div>
+  <Chart />
+</div>
+
+<a class="original" href={original}>View the original</a>
+
+<h2>How it's made</h2>
+
+
+<h2>How I want to make it better</h2>
+
+<style>
+  
+
+  .plate {
+    background-image: url($lib/assets/original-plate-bg.png);
+    background-size: cover;
+    background-repeat: round;
+    padding: 16px;
+    border-radius: 6px;
+    text-transform: uppercase;
+    text-align: center;
+    font-family: "Public Sans", sans-serif;
+    margin: 1vh auto 1vh auto;
+    opacity: 0.9;
+    width: 75%;
+    padding-bottom: 2rem;
+  }
+
+  .chart-title {
+    margin-bottom: 1rem;
+    line-height: 1.2;
+    font-family: "Public Sans", sans-serif;
+    color: black;
+  }
+
+  .chart-title h1 {
+    color: black;
+    font-family: "Public Sans", sans-serif;
+    font-size: 1.1rem;
+  }
+
+  .chart-title h1 {
+    font-weight: 700;
+    margin: 1rem 0 0;
+  }
+
+  .original {
+    font-size: 1rem;
+  }
+
+  @media screen and (max-width: 800px) {
+    .plate {
+      width: 100%;
+    }
+  }
+</style>
---
+++ src/routes/plates/challenge03/Chart.svelte
@@ -0,0 +1,121 @@
+<script>
+
+  import { csv } from 'd3-fetch';
+
+  let data = csv("$lib/data/challengeo3.csv");
+  
+  $inspect(data);
+  let width = 300;
+  $: height = width*1.25;
+  let countiesRewind = rewind(georgiaCounties, { reverse: true });
+  let counties = georgiaCounties.features;
+  let counties2 = georgiaCounties.features;
+
+  // Projection function
+  $: projection = geoAlbers()
+    .scale(3900 - (871 - width)*3)
+    .rotate([79.1+(871-width)/200, 11.75-(871-width)/200])
+    .translate([width / 2.5, height / 2]); // Where the projection is centered
+  $: console.log(path.bounds(counties))
+
+  // Path generator
+  $: path = geoPath().projection(projection);
+
+  let georgiaCoords = [-79.5, 27.5];
+
+  const popCats = ["> 1000", "1000 - 2500", "2500 - 5000", "5000 - 10000", "10000 - 15000", "15000 - 20000", "20000 - 30000", null];
+  const colourRange = ["#516255", "#e4b558", "#df9e98", "#cf354f", "#b5967c", "#654321", "#41377c", "#e4d3bf"];
+  let colourScale = scaleOrdinal()
+    .domain(popCats)
+    .range(colourRange);
+</script>
+
+<div class="chart-container" bind:clientWidth={width}>
+  <div class="row">
+    <div class="chart">
+      <h6 class="map-year">1870</h6>
+      <svg class="left" width={width / 2.5} height={height/3}>
+        <!-- Counties 1870-->
+        {#each counties as county}
+          <!-- svelte-ignore a11y-click-events-have-key-events --->
+          <path
+            class="county"
+            d={path(county.geometry)}
+            fill={colourScale(county.properties.population1870)}
+            stroke="black"
+            tabIndex="0"
+          />
+        {/each}
+  
+      </svg>
+    </div>
+    <!-- Top right legend -->
+    <Legend {colourScale} indexStart={4} indexStop={7} />
+  </div>
+  <div class="row">
+    <!-- Bottom left legend -->
+    <Legend {colourScale} indexStart={0} indexStop={4} right={true}/>
+  <div class="chart">
+    <h6 class="map-year">1880</h6>
+    <svg class="right" width={width / 2.5} height={height/3}>
+      <!-- Counties 1880-->
+      {#each counties as county}
+        <!-- svelte-ignore a11y-click-events-have-key-events --->
+        <path
+          class="county"
+          d={path(county.geometry)}
+          fill={colourScale(county.properties.population1880)}
+          stroke="black"
+          tabIndex="0"
+        />
+      {/each}
+
+    </svg>
+  </div>
+  </div>
+  
+</div>
+
+<style>
+  @import url("https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,100..900;1,100..900&display=swap");
+
+  .chart-container {
+    margin: auto;
+    display: flex;
+    flex-flow: row wrap;
+    gap: 10px;
+  }
+
+  .row {
+    display: flex;
+    flex-flow: row nowrap;
+    gap: 10px;
+    width: 100%;
+    justify-content: center;
+  }
+
+  .chart {
+    flex-direction: column;
+    display: flex;
+    align-items: flex-start;
+  }
+
+  .map-year {
+    margin-top: 0;
+    position: relative;
+    left: 25%;
+  }
+
+  .left {
+    align-self: flex-end;
+  }
+
+  .right {
+    align-self: flex-start;
+  }
+
+  svg {
+    overflow: visible;
+  }
+
+</style>
