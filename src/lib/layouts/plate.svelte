f20babcc91282353c5311edb257058460b0099d7
Mark Barrett <markkbarrett@gmail.com>
2025-06-25 17:20:33 -0400

    Prettify code

--- src/lib/layouts/plate.svelte
+++ src/lib/layouts/plate.svelte
@@ -2,20 +2,15 @@
   import Seo from "$lib/components/Seo.svelte";
   import { siteTitle } from "$lib/constants";
   import { base } from "$app/paths";
-  import {format} from "date-fns";
+  import { format } from "date-fns";
   import { Utterances, utterancesTheme } from "@codewithshin/svelte-utterances";
   import { repoName } from "$lib/constants";
+  import '$lib/css/plate.css';
 
   export let data;
-  export let title,
-    description,
-    date,
-    challenge,
-    plateNum,
-    edit,
-    image;
+  export let title, description, date, challenge, plateNum, edit, image;
 
-  const theme = 'github-light';
+  const theme = "github-light";
 
   const seo = {
     title: `${title} | ${siteTitle}`,
@@ -29,35 +24,40 @@
 
 <Seo {...seo} />
 
-<a class="challenge" href="https://github.com/ajstarks/dubois-data-portraits/tree/master/challenge/2024#the-challenges">Challenge #{challenge}</a>
+<a
+  class="challenge"
+  href="https://github.com/ajstarks/dubois-data-portraits/tree/master/challenge/2024#the-challenges"
+  >Challenge #{challenge}</a
+>
 <h1>{title} (Plate {plateNum})</h1>
 
 <p class="date">{format(actualDate, "d MMM yyyy")}</p>
 
-
 <blockquote>
   <strong>Please Note</strong>: The language used in this piece comes directly
-  from W.E.B. Du Bois' scientific study in 1900. I am torn about reproducing language that could
-  be painful for people to read, but I thought altering the wording would be a
-  disservice to the work. I find the language of Du Bois' work to be a powerful
-  reminder, especially for readers in a modern context, that racial inequality
-  has a long tenure in our societies and institutions. However, race is a
-  difficult subject to navigate with appropriate
-  awareness and compassion, especially for white people like myself. If you have any
-  thoughts or feedback on how to better present this work, <a href="../contact">please reach out</a>.
+  from W.E.B. Du Bois' scientific study in 1900. I am torn about reproducing
+  language that could be painful for people to read, but I thought altering the
+  wording would be a disservice to the work. I find the language of Du Bois'
+  work to be a powerful reminder, especially for readers in a modern context,
+  that racial inequality has a long tenure in our societies and institutions.
+  However, race is a difficult subject to navigate with appropriate awareness
+  and compassion, especially for white people like myself. If you have any
+  thoughts or feedback on how to better present this work,
+  <a href="../contact">please reach out</a>.
 </blockquote>
 
 <slot />
 
-<hr>
+<hr />
 
-<Utterances reponame={ repoName } {theme}/>
+<Utterances reponame={repoName} {theme} />
 
-<a class="return padding-bottom border-top" href="/" sveltekit:prefetch>&#8592; Back to all plates </a>
+<a class="return padding-bottom border-top" href="/" sveltekit:prefetch
+  >&#8592; Back to all plates
+</a>
 <p class="edit"><a href={edit} target="_blank">Edit this page</a></p>
 
 <style>
-
   .challenge {
     font-size: 0.95rem;
     color: var(--db-brown);
@@ -95,14 +95,14 @@
   }
 
   .border-top {
-  border-top: 1px solid var(--accent-color);
-  border-top: 0.5px solid var(--accent-color);
-  padding-top: 1.5rem;
-}
+    border-top: 1px solid var(--accent-color);
+    border-top: 0.5px solid var(--accent-color);
+    padding-top: 1.5rem;
+  }
 
-.padding-bottom {
-  padding-bottom: 0.2rem;
-}
+  .padding-bottom {
+    padding-bottom: 0.2rem;
+  }
 
   blockquote {
     font-size: 0.9rem;
@@ -117,6 +117,6 @@
     h1 {
       font-size: 2rem;
     }
-
   }
+  
 </style>
