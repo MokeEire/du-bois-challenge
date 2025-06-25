74dbc97754d2083a18ca3e24bd3e22b375c471d8
Mark Barrett <markkbarrett@gmail.com>
2025-06-25 17:43:40 -0400

    Fix how pagination works

--- src/lib/components/Pagination/Pagination.svelte
+++ src/lib/components/Pagination/Pagination.svelte
@@ -3,7 +3,7 @@
 
   export let items = [];
   export let currentPage = 1;
-  export let itemsPerPage = 1;
+  export let itemsPerPage = 3;
 
   let numberOfPages = Math.ceil(items.length / itemsPerPage);
 
@@ -11,11 +11,12 @@
     return array.slice((page_number - 1) * page_size, page_number * page_size);
   };
 
-  $: $paginatedPosts = paginate(items, 2, currentPage);
+  $: $paginatedPosts = paginate(items, itemsPerPage, currentPage);
 </script>
 
 <nav>
-  <ul>
+  {#if numberOfPages > 1}
+    <ul>
     {#each Array(numberOfPages) as _, index}
       <li>
         <button
@@ -25,18 +26,34 @@
       </li>
     {/each}
   </ul>
+  {/if}
 </nav>
 
 <style>
   ul {
     display: flex;
     place-content: center;
+    padding: inherit;
+    margin: 0;
   }
   li {
     margin-right: 2px;
+    position: initial;
+    padding: initial;
+    margin: initial;
+    line-height: initial;
   }
   .active {
     color: white;
     background-color: var(--sidebar-bg);
   }
+
+
+
+  /* Custom bullet using Du Bois color palette */
+  li::before {
+    content: "";
+    display: none;
+  }
+
 </style>
