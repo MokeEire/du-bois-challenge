d9cae7872c87fc09dea15b2f9e53d346f280bfe7
Mark B <markkbarrett@gmail.com>
2025-10-07 16:29:55 -0700

    Add links for footer

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -20,3 +20,24 @@ export const siteDescription = "My work for the Du Bois Visualization Challenge.
 
 export const footerText =
   /* <Current Year> is already prefixed */ "All rights reserved";
+
+  export const links = [
+  {
+    title: "Website",
+    url: "https://yuyutsu.vercel.app/",
+    target: "_blank",
+    icon: "arcticons--emoji-web",
+  },
+  {
+    title: "Github",
+    url: "https://github.com/MokeEire/",
+    target: "_blank",
+    icon: "simple-icons--github",
+  },
+  {
+    title: "LinkedIn",
+    url: "https://www.linkedin.com/in/markkbarrett/",
+    target: "_blank",
+    icon: "brandico--linkedin-rect",
+  },
+];
