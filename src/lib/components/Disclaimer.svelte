6e2d6b1e8f34f7af0476412ba88357aa2228183b
Mark B <markkbarrett@gmail.com>
2025-10-08 13:13:51 -0700

    Refactor disclaimer component: improve clarity and structure of text

--- src/lib/components/Disclaimer.svelte
+++ src/lib/components/Disclaimer.svelte
@@ -1,22 +1,34 @@
 <blockquote>
-  <strong>Please Note</strong>: The language used in these pieces comes directly
-  from W.E.B. Du Bois' scientific study in 1900. 
-  
-  I hope this language serves as a powerful reminder that racial inequality has a
-  long tenure in our societies and institutions.
-  However, race is a difficult subject to navigate with appropriate awareness
-  and compassion, especially for white people like myself. If you have any
-  thoughts or feedback on how to better present this work,
-  <a href="../contact">please reach out</a>.
+  <p>
+    <strong>Please Note</strong>: The language used in these pieces comes
+    directly from W.E.B. Du Bois' scientific study in 1900.
+  </p>
+  <p>
+    For white people like myself, it can be a difficult to navigate topics
+    concerning race with appropriate awareness and compassion. I chose to
+    recreate Du Bois' work as faithful to the original as possible because I
+    believe it sharpens the clarity and importance of his work.
+  </p>
+  <p>
+    If you have any thoughts or feedback on the presentation of this work,
+    <a href="../contact">please reach out to me</a>.
+  </p>
 </blockquote>
 
 <style>
   blockquote {
-    font-size: 0.9rem;
     border-left: 12px solid var(--db-crimson);
     border-radius: 2px;
     margin: 1rem 0 1rem;
     background-color: transparent;
     padding: 8px 0 8px 16px;
+  }
+
+  blockquote p {
+    font-size: 0.9rem;
+  }
+
+  blockquote :not(:first-child) {
+    margin-top: 0.5rem;
   }
 </style>
